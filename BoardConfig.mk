4f4ab9dd2ceece8c138691cad928413913d562a7
ArcticAquila <franklinazriel6942@outlook.com>
2024-01-08 13:28:48 +0700

    Add framework, matrix, manifest device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,7 @@ endif
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
