eb4393a38d2d101607584f1a297e7db32cec287f
ArcticAquila <franklinazriel6942@outlook.com>
2024-01-26 22:29:58 +0700

    add some missing file and fixing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,9 @@ endif
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
