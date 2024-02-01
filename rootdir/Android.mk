54d6d2f1762fbc1a1e485d62fe18ee9891798227
ArcticAquila <franklinazriel6942@outlook.com>
2024-02-01 22:06:07 +0700

    Fixing wrong code

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 # Init file
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.connectivity.common.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -9,7 +9,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/factory_init.connectivity.common.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.connectivity.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -17,7 +17,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/factory_init.connectivity.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.project.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -25,7 +25,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/factory_init.project.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := factory_init.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -33,7 +33,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/factory_init.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init_connectivity.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -41,7 +41,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init_connectivity.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.aee.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -49,7 +49,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.aee.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ago.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -57,7 +57,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.ago.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.cgroup.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -65,7 +65,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.cgroup.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.connectivity.common.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -73,7 +73,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.connectivity.common.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.connectivity.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -81,7 +81,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.connectivity.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.modem.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -89,7 +89,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.modem.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6765.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -97,7 +97,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.mt6765.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6765.usb.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -105,7 +105,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.mt6765.usb.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -113,7 +113,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.project.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.sensor_1_0.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -121,7 +121,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/init.sensor_1_0.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.connectivity.common.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -129,7 +129,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.connectivity.common.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.connectivity.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -137,7 +137,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.connectivity.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := meta_init.modem.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -145,7 +145,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.modem.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := meta_init.project.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -153,7 +153,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.project.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := meta_init.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -161,7 +161,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.vendor.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -169,7 +169,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/init/meta_init.vendor.rc
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := multi_init.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -186,7 +186,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := etc/fstab.mt6765
 include $(BUILD_PREBUILT)
 
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := fstab.ramplus
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
@@ -196,10 +196,10 @@ include $(BUILD_PREBUILT)
 
 ## TEMPORARY
 # PLEASE REMOVE AFTER SELINUX WITH ENFORCING IS WORKING!
-include $(CLEAN_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.selinux.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-LOCAL_SRC_FILES    := etc/init.selinux.rc
+LOCAL_SRC_FILES    := etc/init/init.selinux.rc
 include $(BUILD_PREBUILT)
