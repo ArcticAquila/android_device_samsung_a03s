fc5cb1565981a163d9e0d6f94aead2aa36bc613f
ArcticAquila <franklinazriel6942@outlook.com>
2024-02-22 20:01:15 +0700

    Copy firmware to vendor

--- device.mk
+++ device.mk
@@ -60,6 +60,31 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml 
 
+# Prebuilt Firmware
+PRODUCT_COPY_FILES += \
+    vendor/firmware/AbovCapSense.BIN:$(TARGET_COPY_OUT_VENDOR)/firmware/AbovCapSense.BIN \
+    vendor/firmware/Himax_firmware_hx83102.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/Himax_firmware_hx83102.bin \
+    vendor/firmware/Himax_firmware_hx83112.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/Himax_firmware_hx83112.bin \
+    vendor/firmware/Himax_mpfw_hx83102.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/Himax_mpfw_hx83102.bin \
+    vendor/firmware/Himax_mpfw_hx83112.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/Himax_mpfw_hx83112.bin \
+    vendor/firmware/WIFI_RAM_CODE_soc1_0_1c_1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/WIFI_RAM_CODE_soc1_0_1c_1.bin \
+    vendor/firmware/fs18xx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fs18xx.bin \
+    vendor/firmware/gc7202_hsd_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gc7202_hsd_fw.bin \
+    vendor/firmware/gc7202_hsd_mp.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gc7202_hsd_mp.bin \
+    vendor/firmware/hlt_jadard_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/hlt_jadard_firmware.bin \
+    vendor/firmware/ilitek_fw_ls_panda.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ilitek_fw_ls_panda.bin \
+    vendor/firmware/ilitek_fw_txd_boe.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ilitek_fw_txd_boe.bin \
+    vendor/firmware/mdt_jadard_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mdt_jadard_firmware.bin \
+    vendor/firmware/mt6631_fm_v1_coeff.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mt6631_fm_v1_coeff.bin \
+    vendor/firmware/mt6631_fm_v1_patch.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mt6631_fm_v1_patch.bin \
+    vendor/firmware/soc1_0_patch_mcu_1c_1_hdr.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/soc1_0_patch_mcu_1c_1_hdr.bin \
+    vendor/firmware/soc1_0_ram_bt_1c_1_hdr.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/soc1_0_ram_bt_1c_1_hdr.bin \
+    vendor/firmware/soc1_0_ram_mcu_1c_1_hdr.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/soc1_0_ram_mcu_1c_1_hdr.bin \
+    vendor/firmware/soc1_0_ram_wifi_1c_1_hdr.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/soc1_0_ram_wifi_1c_1_hdr.bin \
+    vendor/firmware/txd_hkc_firmware_nl9911c.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txd_hkc_firmware_nl9911c.bin \
+    vendor/firmware/txd_jadard_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txd_jadard_firmware.bin \
+    vendor/firmware/xl_truly_firmware_nl9911c.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/xl_truly_firmware_nl9911c.bin
+
 # Init files
 PRODUCT_PACKAGES += \
     fstab.mt6765 \
